9a667f5c0782eb459659a7c63d3488269d2433a8
kytice <kytice81@gmail.com>
2024-10-28 13:10:25 +0000

    Update package.json dependencies

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("./app_server/models/db");
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -5,7 +7,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 const indexRouter = require("./app_server/routes/index");
-const usersRouter = require("./app_server/routes/users");
 
 var app = express();
 
@@ -20,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
