f74b6ec57571d56babecb2d59914cc1910ea54e9
kytice <kytice81@gmail.com>
2024-11-29 16:55:02 +0000

    add rating-stars component

---
+++ QuotistAngular/quotist-public/src/app/rating-stars/rating-stars.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-rating-stars',
+  templateUrl: './rating-stars.component.html',
+  styleUrls: ['./rating-stars.component.css'],
+})
+export class RatingStarsComponent {
+  @Input() thisRating: number = 0;
+}
