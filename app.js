55635f1342e9d13150a235ce384152194f49ed1d
kytice <kytice81@gmail.com>
2024-11-29 17:25:05 +0000

    add passport.js and ssl

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-require("./app_server/models/db");
+require("./app_api/models/db");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const flash = require("connect-flash");
+const index = require("./app_server/routes/index");
+const apiRoutes = require("./app_api/routes/index");
+const Account = require("./app_api/models/account");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const app = express();
 
-const indexRouter = require("./app_server/routes/index");
-
-var app = express();
-
-// view engine setup
 app.set("views", path.join(__dirname, "app_server", "views"));
 app.set("view engine", "pug");
 
@@ -19,23 +25,53 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "app_public/build")));
 
-app.use("/", indexRouter);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
-// catch 404 and forward to error handler
+passport.use(Account.createStrategy());
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+app.use("/api", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use("/", index);
+app.use("/api", apiRoutes);
+
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
+
+const privateKey = fs.readFileSync("./sslcert/key.pem", "utf8");
+const certificate = fs.readFileSync("./sslcert/cert.pem", "utf8");
+const credentials = { key: privateKey, cert: certificate };
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+httpServer.listen(8080);
+httpsServer.listen(8443);
 
 module.exports = app;
